caf56924af7e0997c001b5e4a17504d3cda3ccd1
ag1rlisagun <aaliyahwusu@gmail.com>
2025-01-18 15:53:14 -0700

    we have images

--- jobapplication/job/urls.py
+++ jobapplication/job/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.job, name="job")
+    path("", views.job, name="job"),
+    path('upload/', views.upload_file, name='upload_file'),
 ]
--- jobapplication/job/views.py
+++ jobapplication/job/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def job(request):
-    return HttpResponse('Hello World')
+    return render(request, "index.html")
+
+def upload_file(request):
+    if request.method == 'POST' and request.FILES['file']:
+        uploaded_file = request.FILES['file']
+        # Do something with the uploaded file
+        return render(request, 'upload_success.html', {'file_name': uploaded_file.name})
+    else:
+        return render(request, 'upload_file.html')
